2d70c86893d82685ac7b080007574acbf936f183
In-Wess-Tation <skolekonto318@gmail.com>
2022-09-13 14:21:50 +0200

    list færdig, mangler styling, detail driller

--- detail.js
+++ detail.js
@@ -1,25 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+let params = new URLSearchParams(window.location.search)
+let movie = params.get("movie")
+console.log(movie)
+
     let boks = document.querySelector(".boks")
     console.log(boks)
-    
-    fetch("https://api.themoviedb.org/3/movie/550?api_key=8aae96e730d41065f7cfa804530c488a")
+
+let imgPath ="https://image.tmdb.org/t/p/original"
+let APIKey = "8aae96e730d41065f7cfa804530c488a"
+let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&language=en-US&page=1`
+   
+    fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data)
     
-    
-    data.genres.forEach(genre => {
-    
-    let nowShowing = localStorage.getItem(genre.id)
-        let section = document.createElement("section")
-        section.classList.add("nowShowing")
-        section.innerHTML = `
-            <p>${genre.name}</p>
-    
-    
-        `
-    
-    
-    boks.append(section)
-    })
+
+
+let movieDetails = document.createElement("section")
+movieDetails.innerHTML = `
+<h2 class="movie__title">${data.results.original_title}</h2>
+<p class="movie__score">${data.results.vote_average}</p>
+<p class="movie__genre">${data.results.genre_ids}</p>
+
+
+<div class="movie__LL">
+<div class="movie__length">
+    <p class="grey">Length</p>
+    <p></p>
+</div>
+<div class="movie__language">
+ <p class="grey">Language</p>
+    <p>${data.results.original_language}</p>
+</div>
+<div class="movie__rating">
+    <p class="grey">Ratin</p>
+    <p>${data.results.adult}</p>
+</div>
+
+
+</div>
+<h3 class="descrip__hedline">Description</h3>
+<p class="descrip__text">${data.results.overview}</p>
+`
+
+
+boks.append(movieDetails)
+  
+})
+})
